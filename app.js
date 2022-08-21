ddce0194e3abc99ffb2420eb9d1ab69f7a9c3d8b
Trey <treybyars93@gmail.com>
2022-08-20 21:30:02 -0400

    added scores

--- app.js
+++ app.js
@@ -29,45 +29,44 @@ const changeTurn = () => {
      state.currentPlayerIndex = state.currentPlayerIndex === 0 ? 1 : 0; //ternary decides player order
 }   
 
+const takeTurn = (boxIndex) => {
+    if(!state.players[0] || !state.players[1]) return;
+
+    if (state.board[boxIndex].isFilled) return;
+    
+     
+    state.board[boxIndex].isFilled = true;
+    state.scores[state.currentPlayerIndex]++;
+
+    changeTurn();
+    }
+    
+
 const renderAll = () => {
     renderBoard();
     renderPlayer();
     renderScore();
 }
-    
-const takeTurn = (boxIndex) => {
-     if (!state.players[0] || !state.players[1]) return;
-     const square = state.board[boxIndex];
-     if(!state.board.isFilled) return; 
+        
+// const checkBoard = () => {
+//     for (let i = 0; i < state.board.length; i ++) {
+//         const square = state.board[i];
+//         if (!square.isFilled) return;
+//     }
+//     state.winner = getWinner();
+// }
 
-    //correct
-    if (square.value = true) { // use this to check for wins later
-        state.scores[state.currentPlayerIndex]++;
-    } else {
-    changeTurn()
-    }
-}
-
-const checkBoard = () => {
-    for (let i = 0; i < state.board.length; i ++) {
-        const square = state.board[i];
-        if (!square.isFilled) return;
-    }
-    state.winner = getWinner();
-}
-
-const getWinner = () => {
-    const [player1Score, player2Score] = state.scores //destructuring
-    let winningPlayerIndex = player1Score > player2Score ? 0 : 
-        player1Score === player1Score ? -1 : 1; // ternary
-    return state.players[winningPlayerIndex];
-}
+// const getWinner = () => {
+//     const [player1Score, player2Score] = state.scores //destructuring
+//     let winningPlayerIndex = player1Score > player2Score ? 0 : 
+//         player1Score === player1Score ? -1 : 1; // ternary
+//     return state.players[winningPlayerIndex];
+// }
 
 // DOM selectors
 const boardElem = document.getElementById('board'); 
 const playerTurnElem = document.getElementById('player-turn');
 const scoreElem = document.getElementById('score');
-// const boxElem = document.getAttributeBy(data-index);
 
 // DOM manipulations
 const renderBoard = () => { 
@@ -76,10 +75,6 @@ const renderBoard = () => {
         const square = state.board[i]; // renames state.board[i]
         const boxElem = document.createElement('div'); // creates div elements
         boxElem.classList.add('box'); // adds 'box class to boxElem
-        
-        if(boxElem.isFilled) {
-            boxElem.innerText = square.value;
-        }
 
         boxElem.dataset.index = i; // adds data-index to divs
         boardElem.appendChild(boxElem); // appends boxElem to boardElem 
@@ -116,58 +111,50 @@ const renderPlayer = () => {
 const renderScore = () => {
     scoreElem.innerHTML = `
     <h3>${state.players[0]}: ${state.scores[0]}</h3>
-    <h3>${state.players[1]}: ${state.scores[1]}</h3>
+    <h3>${state.players[1]}: ${state.scores[1]}</h3>  
     `
 }
 
 function renderMark(boxIndex) {
     for (i = 0; i < board.length; i++);
-
      const boxElem = document.getElementsByClassName('box')[boxIndex];
-    if (state.currentPlayerIndex === 0) {
+
+    if (state.players[0] && state.currentPlayerIndex === 0 && !state.board[boxIndex].isFilled) {
         boxElem.innerText = `X`; 
         boxElem.isFilled = true;
-        state.board[boxIndex] = 'X';
-    } if (state.currentPlayerIndex === 1) {
+
+        takeTurn(boxIndex);
+    
+    } if (state.players[1] && state.currentPlayerIndex === 1 && !state.board[boxIndex].isFilled) {
         boxElem.innerText = `O`;
         boxElem.isFilled = true;
-        state.board[boxIndex] = 'O';
-    }
-    takeTurn();
-    changeTurn();
+        
+        takeTurn(boxIndex);
+
+    } else return 
+    
 }
 
 // event listeners
 
 boardElem.addEventListener('click', (event) => { //event delegation
     if (event.target.className !== 'box') return; //keeps user from clicking board border
-    let boxIndex = event.target.dataset.index; //grabs data-index
-    
-    
-    takeTurn(boxIndex);
-    // checkBoard();
-    // renderAll();
 
-    if(event.target.className === 'box') renderMark(boxIndex);
-       renderPlayer();
-    //    takeTurn();
-    //    changeTurn();
-    // }
-    
-    // takeTurn(boxIndex);
-    // checkBoard();
-    // renderAll();
+    if (event.target.className === 'box') {
+        let boxIndex = event.target.dataset.index; //grabs data-index
 
+        renderMark(boxIndex);
+        renderPlayer();
+        renderScore();
+    } 
 })
 
 playerTurnElem.addEventListener('click', (event) => {
+    if (event.target.className !== 'start') return;
     if (event.target.className === 'restart') {
         resetState();
         renderAll();
-    }
-    if (event.target.className !== 'start') return;
-
-    else {    
+    } else {    
     const player1input = document.getElementsByName('player1')[0];
     state.players[0] = player1input.value;
 
